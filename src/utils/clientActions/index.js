beb789ebcafa4c54563cb4a384638a653108a605
Ian <ishelanskey@gmail.com>
2022-03-04 14:39:01 -0800

    updates client actions - adds photo upload

--- src/utils/clientActions/index.js
+++ src/utils/clientActions/index.js
@@ -1,13 +1,26 @@
-const API_ENDPOINT = "https://73oajmp3pd.execute-api.us-west-1.amazonaws.com/prod"
-const API_KEY = "xOsPBbAKQjAnfmQ11O2haoocwbwXZ7map1cEUcgf"
+const JOURNAL_API_ENDPOINT = "https://73oajmp3pd.execute-api.us-west-1.amazonaws.com/prod"
+const JOURNAL_API_KEY = "xOsPBbAKQjAnfmQ11O2haoocwbwXZ7map1cEUcgf"
+
+const PHOTO_ENDPOINT = "https://8d6ne1wbak.execute-api.us-west-1.amazonaws.com/prod/"
+const PHOTO_API_KEY = "xOsPBbAKQjAnfmQ11O2haoocwbwXZ7map1cEUcgf"
 
 export const SubmitData = (Data)=>{
-    return fetch(API_ENDPOINT, {
+    return fetch(JOURNAL_API_ENDPOINT, {
         method: "POST",
         headers:{
             'Content-Type': 'application/json',
-            'X-Api-Key': API_KEY
+            'X-Api-Key': JOURNAL_API_KEY
         },
         body: JSON.stringify(Data)
     }) 
+}
+
+export const PutImage = (Name, Data)=>{
+    return fetch(PHOTO_ENDPOINT+Name, {
+        method: "PUT",
+        headers:{
+            "x-api-Key": PHOTO_API_KEY
+        },
+        body: Data
+    })
 }
