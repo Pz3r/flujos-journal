f0eeb0f754380e20f8e0c15ea5630d329c675f32
Ian Shelanskey <ishelanskey@gmail.com>
2022-06-20 22:39:42 -0700

    adds translations

---
+++ src/assets/copy.js
@@ -0,0 +1,300 @@
+LandingPage = {
+	Title:{
+		"en": "FLOWS",
+		"es": "Flows",
+		"zh": "",
+	},
+	Location:{
+		"en": "Los Angeles",
+		"es": "Los Angeles",
+		"zh": "洛杉磯",
+	},
+	JournalingMenuHeading:{
+		"en": "Ride Journaling",
+		"es": "Diario de viaje",
+		"zh": "騎行記錄",
+	},
+	LiveModeMenuHeading:{
+		"en": "Live Mode",
+		"es": "Modo en vivo",
+		"zh": "實況模式",
+	},
+	PhotoUploadMenuHeading:{
+		"en": "Photo Upload",
+		"es": "Subir foto",
+		"zh": "照片上載",
+	},
+	ArtworkDetailsMenuHeading:{
+		"en": "Artwork Detail",
+		"es": "Obra de Arte",
+		"zh": "原圖詳細資訊",
+	}
+}
+
+AboutPage = {
+	Title:{
+		"en": "FLOWS",
+		"es": "Flows",
+		"zh": "",
+	},
+	Heading:{
+		"en": "About",
+		"es": "Acerca",
+		"zh": "關於",
+	},
+	p1:{
+		"en": "CiBiC stands for Civic Bicycle Commuting and is a pilot transportation research project to encourage people to try commuting to work on bicycles in groups. It’s like carpooling but on bikes! The goal is to create a community-driven group bicycling system.",
+		"es": "CiBiC: Civic Bicycle Commuting. Es un proyecto de investigación sobre transporte, en fase piloto, que busca impulsar a la gente a trasladarse grupalmente al trabajo en bicicleta.",
+		"zh": "CiBiC 全名為 Civic Bicycle Commuting，是一項試點交通研究專案，旨在鼓勵人們嘗試以自行車團騎方式上下班。這就像拼車共乘，而且是騎自行車！此目標是創建一個以社群力量為主的團體自行車騎乘系統。",
+	},
+	p2:{
+		"en": "CiBiC is a collaborative research project led by UCLA and funded by the National Science Foundation. CiBiC studies how to support bicycle commuting in L.A.",
+		"es": "CiBiC es un proyecto de investigación participativo dirigido por UCLA, con el apoyo financiero de la National Science Foundation. CiBiC estudia cómo apoyar el traslado diario en bicicleta.",
+		"zh": "CiBiC 是由加州大學洛杉磯分校指導，國家科學基金會資助等，一項合作研究的專案。 CiBiC 研究如何扶助支援洛杉磯的自行車通勤。",
+	},
+	p3:{
+		"en": "To learn more about getting involved with CiBiC please visit our website!",
+		"es": "Para conocer más acerca de cómo participar en CiBiC ¡visita nuestro Website!",
+		"zh": "若您想瞭解更多有關參與 CiBiC 的資訊，請造訪我們的網站！",
+	},
+	LocationHeading:{
+		"en": "Key Location",
+		"es": "Locación principal",
+		"zh": "主要位置",
+	},
+	Location:{
+		"en": "Los Angeles State Historic Park",
+		"es": "Los Angeles State Historic Park",
+		"zh": "洛杉磯州立歷史公園",
+	},
+	Street:{
+		"en": "1245 N Spring St,",
+		"es": "1245 N Spring St,",
+		"zh": "1245 N Spring St,",
+	},
+	City:{
+		"en": "Los Angeles, CA 90012",
+		"es": "Los Angeles, CA 90012",
+		"zh": "Los Angeles, CA 90012",
+	},
+	Footer:{
+		"en": "Exhibition dates coming soon!",
+		"es": "¡Fechas de exhibición pronto!",
+		"zh": "展覽會日期即將公開！",
+	}
+}
+
+
+Satisfaction = {
+	Prompt:{
+		"en": "Rate your commute satisfaction:",
+		"es": "Evalúa tu satisfacción en el traslado",
+		"zh": "評價您的通勤滿意度：",
+	},
+	Options:[
+		{
+			"en": "Terrible",
+			"es": "Terrible",
+			"zh": "糟透了",
+		},
+		{
+			"en": "Bad",
+			"es": "Malo",
+			"zh": "不太好",
+		},
+		{
+			"en": "Okay",
+			"es": "Okay",
+			"zh": "還可以",
+		},
+		{
+			"en": "Good",
+			"es": "Bueno",
+			"zh": "蠻不錯",
+		},
+		{
+			"en": "Great",
+			"es": "Genial",
+			"zh": "非常棒",
+		}
+	]
+}
+
+RideCharacteristics = {
+	Prompt:{
+		"en": "Select all the characteristics of your ride:",
+		"es": "Selecciona las características de tu ride",
+		"zh": "選取您騎行中所有的特色：",
+	},
+	Options:[
+		{
+			"en": "Healthy",
+			"es": "Saludable",
+			"zh": "健康",
+		},
+		{
+			"en": "Safe",
+			"es": "Seguro",
+			"zh": "安全",
+		},
+		{
+			"en": "Playful",
+			"es": "Divertido",
+			"zh": "有趣",
+		},
+		{
+			"en": "Relaxing",
+			"es": "Relajante",
+			"zh": "舒壓",
+		},
+		{
+			"en": "Liberating",
+			"es": "Liberador",
+			"zh": "自由",
+		},
+		{
+			"en": "Easy",
+			"es": "Simple",
+			"zh": "輕鬆",
+		},
+		{
+			"en": "Slow",
+			"es": "Lento",
+			"zh": "耗時",
+		},
+		{
+			"en": "Hazardous",
+			"es": "Peligroso",
+			"zh": "有害",
+		},
+		{
+			"en": "Tiring",
+			"es": "Cansado",
+			"zh": "疲憊",
+		},
+		{
+			"en": "Stressful",
+			"es": "Estresante",
+			"zh": "壓力",
+		},
+		{
+			"en": "Rushed",
+			"es": "Apurado",
+			"zh": "倉促",
+		},
+		{
+			"en": "Unsafe",
+			"es": "Inseguro",
+			"zh": "危險",
+		}
+	]
+}
+
+ShortResponse = {
+	Prompt:{
+		"en": "Describe your ride with one word or short phrase:",
+		"es": "Describe tu viaje en una palabra o frase corta",
+		"zh": "用一個字或片語描述說明您的騎行：",
+	},
+	Placeholder:{
+		"en": "My ride was...",
+		"es": "My viaje fue...",
+		"zh": "我的騎行是...",
+	}
+}
+
+Color = {
+	Prompt:{
+		"en": "What color best expresses how you feel about your last CiBiC ride?",
+		"es": "¿Qué color expresa cómo te sientes con tu último viaje con CiBiC?",
+		"zh": "哪一種顏色最能表達您對於最近一次 CiBiC 騎行的感受？",
+	},
+	cta:{
+		"en": "Tap on a color",
+		"es": "Presiona en un color",
+		"zh": "輕觸其一顏色",
+	}
+}
+
+Photos = {
+	Prompt:{
+		"en": "Upload photos of your ride:",
+		"es": "Sube fotos de tu viaje",
+		"zh": "上載您騎行的照片：",
+	},
+	optional:{
+		"en": "Optional",
+		"es": "Opcional",
+		"zh": "自由選項",
+	},
+	upload:{
+		"en": "Upload",
+		"es": "Subir",
+		"zh": "上載",
+	},
+	cancel:{
+		"en": "Cancel",
+		"es": "Cancelar",
+		"zh": "取消",
+	}
+}
+
+ThankYou = {
+	header:{
+		"en": "Thank you!",
+		"es": "Gracias!",
+		"zh": "謝謝您！",
+	},
+	information:{
+		"en": "Your submissions were added to our CiBiC cartography system.",
+		"es": "Tu envío fue agregado a nuestro sistema cartográfico de CiBiC",
+		"zh": "您傳送的資料已新增至我們的 CiBiC 製圖系統。",
+	},
+	caption:{
+		"en": "A recent snapshot of the system.",
+		"es": "Una captura reciente del sistema",
+		"zh": "系統的最新快照。",
+	}
+}
+
+Live = {
+	header:{
+		"en": "Live",
+		"es": "En Vivo",
+		"zh": "實況",
+	},
+	caution:{
+		"en": "Caution",
+		"es": "Precaución",
+		"zh": "注意",
+	},
+	community:{
+		"en": "Community",
+		"es": "Comunidad",
+		"zh": "社群",
+	},
+	positivity:{
+		"en": "Positivity",
+		"es": "Positividad",
+		"zh": "積極性",
+	}
+}
+
+QRCode = {
+	header:{
+		"en": "Your QR Code",
+		"es": "Tu QR code",
+		"zh": "您專屬的 QR 代碼",
+	},
+	information:{
+		"en": "Use this QR code at the nearest CiBiC exhibition to see a custom visualization of your rides. Check the About page to see locations and exhibition dates.",
+		"es": "Utilice este código QR en la exposición CiBiC más cercana para ver una visualización personalizada de tus viajes. Consulta la página para saber más acerca de para ver las ubicaciones y las fechas de exposición.",
+		"zh": "在您鄰近的 CiBiC 展覽會中使用此 QR 代碼，即可觀賞您騎行的自訂視覺化效果。查看關於頁面了解展覽會地點和日期。",
+	},
+	download:{
+		"en": "Download QR",
+		"es": "Descarga el QR",
+		"zh": "下載 QR",
+	}
+}
+
