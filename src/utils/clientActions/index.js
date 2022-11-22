88d159b0917caea87b7c1b53ccd9709aaca597a3
Ian <ishelanskey@gmail.com>
2022-11-22 09:42:19 -0800

    updates to proxy url

--- src/utils/clientActions/index.js
+++ src/utils/clientActions/index.js
@@ -1,8 +1,8 @@
 const JOURNAL_API_ENDPOINT = "https://73oajmp3pd.execute-api.us-west-1.amazonaws.com/prod"
 const JOURNAL_API_KEY = "xOsPBbAKQjAnfmQ11O2haoocwbwXZ7map1cEUcgf"
 
-const RIDE_WITH_GPS_ENDPOINT = "https://www.ridewithgps.com/current.json"
-const RIDE_WITH_GPS_API_KEY = "2b2d68c9"
+const RIDE_WITH_GPS_ENDPOINT = "https://o227cwnhkmu6elvr3zbekhviby0zvvbu.lambda-url.us-east-1.on.aws"
+const RIDE_WITH_GPS_API_KEY = "xOsPBbAKQjAnfmQ11O2haoocwbwXZ7map1cEUcgf"
 
 
 const PHOTO_ENDPOINT = "https://rme2z0gw62.execute-api.us-west-1.amazonaws.com/prod/"
@@ -40,7 +40,7 @@ export const PutImage = (Name, Data)=>{
 
 export const LoginRWGPS = (Email, Password)=>{
     //https://ridewithgps.com/api
-    const login_creds = '?email='+encodeURIComponent(Email)+"&password="+encodeURIComponent(Password)+"&apikey="+RIDE_WITH_GPS_API_KEY+"&version=2"
+    var login_creds = '?username='+encodeURIComponent(Email)+"&password="+encodeURIComponent(Password)
     return fetch(RIDE_WITH_GPS_ENDPOINT+login_creds).then((response)=>{
         return response.json()
     })
